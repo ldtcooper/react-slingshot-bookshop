bb3461373a121b2d2054e01586be4313df12ad95
Logan Cooper <ldtcoop@gmail.com>
2017-11-11 11:05:09 -0800

    COmplete navbar

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,17 @@ const App = (props) => {
           <div className="navbar-header">
             <a className="navbar-brand" href="#">Scotch Books</a>
           </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/book">Book</Link></li>
+              <li><Link to="/cart">Cart</Link></li>
+            </ul>
+          </div>
         </div>
       </nav>
+      {props.children}
     </div>
   );
 };
