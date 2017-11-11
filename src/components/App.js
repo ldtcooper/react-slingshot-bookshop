fa53eddc0df08cce83431c1c3381bdf7d68a5a6b
Logan Cooper <ldtcoop@gmail.com>
2017-11-11 11:00:52 -0800

    Build header

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const App = (props) => {
+  return(
+    <div className="container">
+      <nav className="navbar navbarDefault">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Scotch Books</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
