451e6ba286d6887cc71a055b74594aef81656cff
Logan Cooper <ldtcoop@gmail.com>
2017-11-13 14:27:27 -0800

    Add provider

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import { render } from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
 
 render(
-  <Router routes={routes} history={browserHistory}/>,
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory}/>
+  </Provider>,
   document.getElementById('app')
 );
