c864cba5a02ad4d54696b45e599dfaa7a3486c1c
Logan Cooper <ldtcoop@gmail.com>
2017-11-13 14:07:57 -0800

    Add book reducer

---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,14 @@
+import { CREATE_BOOK } from '../actions/bookActions';
+
+const bookReducer = (state=[], action) => {
+  switch (action.type) {
+    case CREATE_BOOK:
+      return(
+        [...state, Object.assign({}, action.book)]
+      );
+    default:
+      return state;
+  }
+};
+
+export default bookReducer;
